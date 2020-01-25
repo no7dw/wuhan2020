a65f4f00b129383ad1adc86b158c1ca17fc32340
no7dw <no7david@gmail.com>
2020-01-26 02:51:43 +0800

    save file utf8

--- crawl.py
+++ crawl.py
@@ -6,15 +6,14 @@ url="https://3g.dxy.cn/newh5/view/pneumonia?from=timeline&isappinstalled=0"
 
 def crawl_job():
     now = datetime.now()
-    time_now = now.strftime("%m%d%Y%H:%M:%S")
     time_hour = now.strftime("%Y%m%d%H")
     response = requests.get(url)
     if response.status_code == 200:
         content = response.content
         time_file = "/tmp/wuhan"+time_hour+".html"
         with open(time_file, 'w') as file:
-            file.write(str(content))
-        print(time_file + " normal saved")  
+            file.write(content.decode("utf-8"))
+        print(time_file + " saved")  
 
 def main():
     schedule.every(1).hours.do(crawl_job)
