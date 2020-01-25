f76002dd9d3a8f9a2fd907272ac23ea6b9a31aa7
no7dw <no7david@gmail.com>
2020-01-26 03:38:53 +0800

    add for anaylse

--- crawl.py
+++ crawl.py
@@ -3,6 +3,25 @@ from datetime import datetime
 import schedule
 import time
 url="https://3g.dxy.cn/newh5/view/pneumonia?from=timeline&isappinstalled=0"
+from lxml import html
+
+def analyse(content,time_hour):
+    # 全国 确诊 1409 例 疑似 2032 例
+    # 死亡 42 例 治愈 39 例
+    tree = html.fromstring(content)
+    c_confirm = tree.xpath('//*[@id="root"]/div/div[3]/div[1]/p[2]/span/span[1]/text()')
+    c_suspect = tree.xpath('//*[@id="root"]/div/div[3]/div[1]/p[2]/span/span[2]/text()')
+    c_dead = tree.xpath('//*[@id="root"]/div/div[3]/div[1]/p[2]/span/span[3]/text()')
+    c_heal = tree.xpath('//*[@id="root"]/div/div[3]/div[1]/p[2]/span/span[4]/text()')
+    import csv
+    nms = [time_hour,c_confirm[0],c_suspect[0],c_dead[0],c_heal[0]]
+    print(nms)
+    results=[[int(i) for i in nms]]
+    f = open('numbers.csv', 'a+')
+    with f:
+        writer = csv.writer(f)
+        writer.writerows(results)
+    
 
 def crawl_job():
     now = datetime.now()
@@ -14,8 +33,10 @@ def crawl_job():
         with open(time_file, 'w') as file:
             file.write(content.decode("utf-8"))
         print(time_file + " saved")  
+        analyse(content, time_hour)
 
 def main():
+    crawl_job()
     schedule.every(1).hours.do(crawl_job)
     while True:
         schedule.run_pending()
